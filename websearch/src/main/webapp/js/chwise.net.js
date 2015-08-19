07ce797c4d617e9bad0532d51666c3cb5cd75427
Alexander Savochkin <AlexanderSavochkin@users.noreply.github.com>
2015-08-18 21:17:13 -0700

    Waiting indicator for search results after new query

--- websearch/src/main/webapp/js/chwise.net.js
+++ websearch/src/main/webapp/js/chwise.net.js
@@ -271,8 +271,32 @@ ChWiSe.Router = Backbone.Router.extend({
 
     ChWiSe.Models.currentResultNumber = 0;
 
+    //Setup waiting indicator
+    var opts = {
+        lines: 5, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 10, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+
+    var spinner = new Spinner(opts);
+    //Start wait indicator
+    var target = document.getElementById('results-list'); //TODO: Use jQuery-style
+    spinner.spin(target);    
     ChWiSe.Models.searchResults.fetch({success: function() {
-      //
+      spinner.stop();
     }});
 
     ChWiSe.Views.view.infiniScroll.resetScroll();  
